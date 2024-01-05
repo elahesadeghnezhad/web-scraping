15aa5aee415f5ffaab5e0b1d3ab25b0f2c2bec34
elahesadeghnezhad <151626576+elahesadeghnezhad@users.noreply.github.com>
2024-01-06 00:39:46 +0330

    Update os_p.py
    using BeautifulSoup and Finding the vector graphics categories

--- os_p.py
+++ os_p.py
@@ -9,3 +9,8 @@ proxies = {
 url = "https://pixabay.com/vectors/"
 responds = requests.get(url=url,proxies=proxies)
 print(responds.content)
+soup = BeautifulSoup(response.text, "html.parser")
+categories = soup.find_all("a", class_="category")
+for category in categories:
+        category_name = category.text.strip()
+        category_link = category["href"]
