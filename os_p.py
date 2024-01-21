6f211c06e9c8e20aa10b2b5af2ef1c1372f6cce0
elahesadeghnezhad <151626576+elahesadeghnezhad@users.noreply.github.com>
2024-01-21 15:59:53 +0330

    Update os_p.py
    add api key and headers

--- os_p.py
+++ os_p.py
@@ -5,12 +5,12 @@ proxies = {
     'http': 'http://127.0.0.1:2081',
     'https': 'http://127.0.0.1:2081',
 }
+api_key = "41944365-6514a9c6e238a11db6ad51ba1"
+headers = {
+    'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/91.0.4472.124 Safari/537.36',
+}
 
-url = "https://pixabay.com/vectors/"
+url = "https://pixabay.com/"
 responds = requests.get(url=url,proxies=proxies)
 print(responds.content)
 soup = BeautifulSoup(response.text, "html.parser")
-categories = soup.find_all("a", class_="category")
-for category in categories:
-        category_name = category.text.strip()
-        category_link = category["href"]
