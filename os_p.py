c7836d0e127e4466c09899634b6c6609d11b37f8
fatemeheydari80 <155193804+fatemeheydari80@users.noreply.github.com>
2024-01-23 03:17:18 -0500

    Update os_p.py

--- os_p.py
+++ os_p.py
@@ -1,8 +1,8 @@
 import requests
-# from bs4 import BeautifulSoup
+from bs4 import BeautifulSoup
 import csv
 import os
-from threading import Thread
+from threading import Thread, Lock
 
 urlll = "https://pixabay.com/api/"
 num_images = 10
@@ -12,7 +12,8 @@ params = {
     "image_type": "vector",
     "per_page": num_images
 }
-# scrape from pixabay
+thread_lock = Lock()
+
 def scrape(urlll, params):
     response = requests.get(urlll, params=params)
     data = response.json()
@@ -49,21 +50,17 @@ def download_image(image_url, image_title, category_directory):
     print(f"Downloaded image: {image_title}")
 
 def write_metadata(title, url, directory, user_id):
-    print(f"Writing metadata for: {title}")
     csv_file = os.path.join(directory, "metadata.csv")
     is_file_exist = os.path.isfile(csv_file)
-
-    try:
-        with open(csv_file, "a", newline="") as file:
-            writer = csv.writer(file)
-
-            # Write header if the file doesn't exist
-            if not is_file_exist:
-                writer.writerow(["Title", "URL", "user_id"])
-
-            # Write metadata
-            writer.writerow([title, url, user_id])
-    except Exception as e:
-        print(f"Error writing metadata to CSV: {e}")
+    
+    with open("csv_file", "a", newline="") as file:
+        writer = csv.writer(file)
         
+        # Write header if the file doesn't exist
+        if not is_file_exist:
+            writer.writerow(["Title", "URL"])
+        
+        # Write metadata
+        writer.writerow([ title, url])
+    
 scrape(urlll,params)
